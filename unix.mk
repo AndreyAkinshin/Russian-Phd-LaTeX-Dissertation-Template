8241eee3bac0b1d34cae384b79e11019b2901e57
Matsievskiy S.V <matsievskiysv@gmail.com>
2019-03-15 16:08:07 +0300

    move rules to unix.mk

--- unix.mk
+++ unix.mk
@@ -1,5 +1,33 @@
 # Данный файл будет добавлен к рабочему Makefile,
 # если операционная система определится как система симейства UNIX
 
-FONTFAMILY ?= 2
+### Пользовательские настройки
 
+FONTFAMILY ?= 2 # Используются шрифты семейства Liberation
+
+### Пользовательские правила
+
+INDENT_SETTINGS ?= indent.yaml
+INDENT_DIRS ?= Dissertation Presentation Synopsis
+INDENT_FILES ?= $(foreach dir,$(INDENT_DIRS),$(wildcard $(dir)/*.tex))
+indent:
+	@$(foreach file, $(INDENT_FILES),\
+	latexindent -l=$(INDENT_SETTINGS) -s -w $(file);)
+
+preformat: synopsis-preformat dissertation-preformat
+
+mylatexformat.ltx:
+	etex -ini "&latex" $@ """$(TARGET)"""
+
+dissertation-preformat: TARGET=dissertation
+dissertation-preformat: mylatexformat.ltx dissertation
+
+dissertation-formated: dissertation
+
+synopsis-preformat: TARGET=synopsis
+synopsis-preformat: mylatexformat.ltx synopsis
+
+synopsis-formated: synopsis
+
+.PHONY: indent preformat dissertation-preformat \
+dissertation-formated synopsis-preformat synopsis-formated
--- usercfg.mk
+++ usercfg.mk
@@ -1,15 +1,41 @@
-### user settings and recipes may be placed in this file
+### Пользовательские настройки
+# Откомментируйте настройки, чтобы использовать их по умолчанию
 
-INDENT_SETTINGS ?= indent.yaml
-INDENT_DIRS ?= Dissertation Presentation Synopsis
-INDENT_FILES ?= $(foreach dir,$(INDENT_DIRS),$(wildcard $(dir)/*.tex))
-indent:
-	@$(foreach file, $(INDENT_FILES),\
-	latexindent -l=$(INDENT_SETTINGS) -s -w $(file);)
+# MKRC ?= latexmkrc # config file
+# TARGET ?= dissertation # target .tex file
+# BACKEND ?= -pdfxe
+## -pdf=pdflatex
+## -pdfdvi=pdflatex with dvi
+## -pdfps=pdflatex with ps
+## -pdfxe=xelatex with dvi (faster than -xelatex)
+## -xelatex=xelatex without dvi
+## -pdflua=lualatex with dvi  (faster than -lualatex)
+## -lualatex=lualatex without dvi
 
-COMPRESS_FILES ?= $(wildcard *.pdf)
+# DRAFTON ?= # 1=on;0=off
+# FONTFAMILY ?= # 0=CMU;1=MS fonts;2=Liberation fonts
+# ALTFONT ?= # 0=Computer Modern;1=pscyr;2=XCharter
+# USEBIBER ?= # 0=bibtex8;1=biber
+# IMGCOMPILE ?= # 1=on;0=off
+# LATEXFLAGS ?= -halt-on-error -file-line-error
+# LATEXMKFLAGS ?= -silent
+# BIBERFLAGS ?= # --fixinits
+# REGEXDIRS ?= . Dissertation Synopsis Presentation # distclean dirs
+
+### Пользовательские правила
+
+COMPRESS_FILE ?= dissertation.pdf
 COMPRESSION_LEVEL ?= default # Possible values: screen, default, ebook, printer, prepress
-compress:
-	@$(foreach file, $(COMPRESS_FILES),\
-	ps2pdf14 -dBATCH -dNOPAUSE -dPDFSETTINGS=/$(COMPRESSION_LEVEL) \
-	-sOutputFile=$(patsubst %.pdf,%_compressed.pdf,$(file)) $(file);)
+COMPRESSION_FLAGS += -dBATCH -dNOPAUSE # no stops
+COMPRESSION_FLAGS += -dEmbedAllFonts=true -dSubsetFonts=true # font settings
+
+# image compression control
+COMPRESSION_FLAGS += -dColorImageDownsampleType=/Average -dColorImageResolution=144
+COMPRESSION_FLAGS += -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=144
+COMPRESSION_FLAGS += -dMonoImageDownsampleType=/Subsample -dMonoImageResolution=144
+
+compress: $(COMPRESS_FILE)
+	ps2pdf14 $(COMPRESSION_FLAGS) -dPDFSETTINGS=/$(COMPRESSION_LEVEL) \
+	-sOutputFile=$(patsubst %.pdf,%_compressed.pdf,$^) $^
+
+.PHONY: compress
