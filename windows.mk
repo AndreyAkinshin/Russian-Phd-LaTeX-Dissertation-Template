e6c60866e69ce93774c31579e3fb9ba1d0854a29
Igor Varfolomeev <ivarfolomeev@slb.com>
2019-03-17 20:47:13 +0300

    indent для Widows (просто раскомментирован)

--- windows.mk
+++ windows.mk
@@ -7,12 +7,12 @@ FONTFAMILY ?= 1 # Используются шрифты семейства MS
 
 ### Пользовательские правила
 
-# INDENT_SETTINGS ?= indent.yaml
-# INDENT_DIRS ?= Dissertation Presentation Synopsis
-# INDENT_FILES ?= $(foreach dir,$(INDENT_DIRS),$(wildcard $(dir)/*.tex))
-# indent:
-# 	@$(foreach file, $(INDENT_FILES),\
-# 	latexindent -l=$(INDENT_SETTINGS) -s -w $(file) &&)\
-# 	echo "done"
+INDENT_SETTINGS ?= indent.yaml
+INDENT_DIRS ?= Dissertation Presentation Synopsis
+INDENT_FILES ?= $(foreach dir,$(INDENT_DIRS),$(wildcard $(dir)/*.tex))
+indent:
+	@$(foreach file, $(INDENT_FILES),\
+	latexindent -l=$(INDENT_SETTINGS) -s -w $(file) &&)\
+	echo "done"
 
 .PHONY: indent
