9049edae6e8ecb3afd708f587a14c034c8f8a030
Igor Varfolomeev <ivarfolomeev@slb.com>
2019-05-04 20:34:29 +0300

    check-bib-dupes-and-usage.py : косметические изменения в соответствии с PEP8

--- biblio/check-bib-dupes-and-usage.py
+++ biblio/check-bib-dupes-and-usage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import glob
+import os
 import re
+
 bibfile = "external.bib"
 authors = {}
 author_list = []
@@ -11,10 +14,10 @@ print("Searching for duped bib records...")
 with open(bibfile, 'r', encoding="utf8") as biblio:
     for bib_line in biblio:
         if "@" in bib_line and "@comment" not in bib_line.lower():
-            if "@" in bib_line.split("{",1)[0]:
-                cite = bib_line.split("{",1)[1].split(",",1)[0]
+            if "@" in bib_line.split("{", 1)[0]:
+                cite = bib_line.split("{", 1)[1].split(",", 1)[0]
                 cleanup = cite.split(' ')
-                cite  = ""
+                cite = ""
                 for e in cleanup:
                     cite += e
                 cites.append(cite)
@@ -34,14 +37,14 @@ with open(bibfile, 'r', encoding="utf8") as biblio:
             bib_line = bib_line.split("author")[1]
             if "=" not in bib_line:
                 continue
-            all_auth = re.split('[{"]', bib_line, maxsplit = 1)[1]
+            all_auth = re.split('[{"]', bib_line, maxsplit=1)[1]
             if "\n" in all_auth:
                 all_auth = all_auth[:-1]
             all_auth = all_auth.split(" and ")
             for sub_auth in all_auth:
-                sub_sub_auth = re.split('[,\.\ \-]', sub_auth)
+                sub_sub_auth = re.split('[,. -]', sub_auth)
                 for name in sub_sub_auth:
-                    cleanup = re.findall('\w+', name)
+                    cleanup = re.findall(r'\w+', name)
                     name = ""
                     for e in cleanup:
                         name += e
@@ -49,24 +52,26 @@ with open(bibfile, 'r', encoding="utf8") as biblio:
                         if name not in author_list:
                             author_list.append(name)
             # print(author_list)
-        if ("pages" in bib_line and "numpages" not in bib_line) or ("article-number" in bib_line) or ("isbn" in bib_line) :
-            pages = re.findall('\d+', bib_line)
+        if ("pages" in bib_line and "numpages" not in bib_line) or ("article-number" in bib_line) or (
+                "isbn" in bib_line):
+            pages = re.findall(r'\d+', bib_line)
             # print(pages)
 for author in authors:
     if len(authors[author]) != len(set(authors[author])):
-        print("\tDuplicated record author:",author)
+        print("\tDuplicated record author:", author)
         prev_page = ""
         for page in authors[author]:
             if page == prev_page:
-                if page == "000": page = "No page"
-                print("\t\t with page:",page)
+                if page == "000":
+                    page = "No page"
+                print("\t\t with page:", page)
             prev_page = page
 
-print("Total cites: ",len(cites))
-import sys,os,shutil,glob,subprocess
+print("Total cites: ", len(cites))
+
 path = os.getcwd()
 path_fig = os.path.join(path, '../Dissertation')
-print("Dissertation path: ",path_fig)
+print("Dissertation path: ", path_fig)
 os.chdir(path_fig)
 files = []
 for fname in glob.iglob('*.tex'):
@@ -79,7 +84,7 @@ for filename in files:
 
 path = os.getcwd()
 path_fig = os.path.join(path, '../common')
-print("common path: ",path_fig)
+print("common path: ", path_fig)
 os.chdir(path_fig)
 files = []
 for fname in glob.iglob('*.tex'):
@@ -89,8 +94,7 @@ for filename in files:
     with open(filename, 'r', encoding="utf8") as myfile:
         all_text += myfile.read().replace('\n', '')
 
-
 print(len(all_text))
 for cite in cites:
     if cite not in all_text:
-        print("Cite "+cite+" is not used")
+        print("Cite " + cite + " is not used")
